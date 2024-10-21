2100dbc5051c7471ac0593eb4d9f5bb16462dd9b
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-10-21 22:01:08 +0700

    load river path

--- utilities/generate_scripts.py
+++ utilities/generate_scripts.py
@@ -12,6 +12,7 @@ DEFAULT_HEADER = """#!/bin/bash
 #SBATCH --mem=<<memory>>
 #SBATCH --cpus-per-task=<<cpus>>
 set -e
+source ~/.river.sh
 """
 ACCESS_HEADER = """
 PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
