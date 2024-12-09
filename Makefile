0141296aa2f3e5af3306215c70548b94e76d59d1
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-12-09 23:16:17 +0700

    hotfix for env

--- Makefile
+++ Makefile
@@ -9,8 +9,11 @@ dev:
 	. env/bin/activate
 	pip install -e .
 
-test:
-	RIVER_HOME="./tests/river_home" pytest --cov=src --cov-report=term tests
+test-install:
+	RIVER_HOME="./tests/river_home_test_setup" pytest -k "setup" --cov=src --cov-report=term
+
+test-base:
+	RIVER_HOME="./tests/river_home" pytest --cov=src -k "not setup" --cov-report=term tests/
 
 # Build the project
 build:
--- tests/river_home/.river.sh
+++
@@ -1,9 +0,0 @@
-
-export RIVER_HOME="./tests/river_home"
-export RIVER_HOME_TOOLS=${RIVER_HOME}/.river/tools
-export MAMBA_ROOT_PREFIX=${RIVER_HOME}/.river/.images/micromamba
-export SINGULARITY_CACHE_DIR=${RIVER_HOME}/.river/.images/singularities
-export NXF_SINGULARITY_CACHEDIR=$SINGULARITY_CACHE_DIR
-export PATH=${RIVER_HOME_TOOLS}:${RIVER_HOME_TOOLS}/openvscode-server-v1.93.1-linux-x64/bin:$/home/giangnguyen/Documents/dev/river-utils/env/bin:$PATH
-eval "$(micromamba shell hook -s posix)"
-micromamba activate -n river
