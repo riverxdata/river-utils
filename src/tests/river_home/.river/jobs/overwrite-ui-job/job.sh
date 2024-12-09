0cafde40b9a3986a7098f40bdf8f1f5ab34e4680
Thanh Giang Nguyen Tan <64969412+nttg8100@users.noreply.github.com>
2024-12-10 01:07:09 +0700

    RC-00 reorg to use pyproject  (#14)

--- src/tests/river_home/.river/jobs/overwrite-ui-job/job.sh
+++ src/tests/river_home/.river/jobs/overwrite-ui-job/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=uuid
 #SBATCH --time=1:00:00
-#SBATCH --output=./tests/river_home/.river/jobs/uuid/job.log
+#SBATCH --output=./src/tests/river_home/.river/jobs/uuid/job.log
 #SBATCH --mem=1G
 #SBATCH --cpus-per-task=1
 source ~/.river.sh
@@ -10,11 +10,11 @@ source ~/.river.sh
 echo <<uuid_job_id>> > <<river_home>>/.river/jobs/<<uuid_job_id>>/job.url
 
 # Symlink analysis
-ln -sf ./tests/river_home/.river/tools/<<analysis>> ./tests/river_home/.river/jobs/uuid/<<analysis>>
+ln -sf ./src/tests/river_home/.river/tools/<<analysis>> ./src/tests/river_home/.river/jobs/uuid/<<analysis>>
 
 # Access job
-echo $(python3 -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()") > ./tests/river_home/.river/jobs/uuid/job.port
-echo $(hostname) > ./tests/river_home/.river/jobs/uuid/job.host
+echo $(python3 -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()") > ./src/tests/river_home/.river/jobs/uuid/job.port
+echo $(hostname) > ./src/tests/river_home/.river/jobs/uuid/job.host
 
 
 # Cloud storage
@@ -22,7 +22,7 @@ trap 'umount $MOUNT_POINT || "S3 bucket is not mounted' EXIT
 set -euo pipefail
 
 # Mount using goofys
-MOUNT_POINT=./tests/river_home/.river/jobs/uuid/workspace 
+MOUNT_POINT=./src/tests/river_home/.river/jobs/uuid/workspace 
 goofys --profile bucket_name --file-mode=0700 --dir-mode=0700 --endpoint=endpoint bucket_name $MOUNT_POINT
 
 # Main
