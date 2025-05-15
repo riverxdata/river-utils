98d99d70014bb89f8464fbe82c0454b8cce99113
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-05-15 21:39:05 +0700

    add nextflow work on the river home

--- install/setup.sh
+++ install/setup.sh
@@ -81,6 +81,7 @@ export RIVER_HOME_TOOLS=\${RIVER_HOME}/.river/bin
 export MAMBA_ROOT_PREFIX=\${RIVER_HOME}/.river/images/micromamba
 export SINGULARITY_CACHE_DIR=\${RIVER_HOME}/.river/images/singularities
 export NXF_SINGULARITY_CACHEDIR=\$SINGULARITY_CACHE_DIR/images
+export NXF_WORK=\${RIVER_HOME}/.river/nextflow/work
 export PATH=\${RIVER_HOME_TOOLS}:\$PATH
 eval "\$(micromamba shell hook -s posix)"
 micromamba activate -n river
